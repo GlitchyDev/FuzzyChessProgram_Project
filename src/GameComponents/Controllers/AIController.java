2d8dfcfe66c034209405522f09be202656a43fb4
evanngevara <44121005+evanngevara@users.noreply.github.com>
2020-03-11 19:51:42 -0400

    Updated AIController
    The AIController performs 2 random moves, creates a GameState branch triggered by each action, one at a time, and evaluates each branch. After the 2 branches for 1 AIController turn are created and evaluated, the GameState is sent to the evaluator again to evaluate it with respect to the player (WHITE) pieces.

--- src/GameComponents/Controllers/AIController.java
+++ src/GameComponents/Controllers/AIController.java
@@ -1,31 +1,44 @@
 package GameComponents.Controllers;
 
+import GameComponents.Board.GameTeam;
 import GameComponents.Board.Pieces.GamePiece;
+import GameComponents.Board.Pieces.GamePieceType;
 import GameComponents.Board.Turn.Action;
 import GameComponents.GUIRenderer;
 import GameComponents.GameState;
 
+
 public class AIController {
+
+    static int numOfStepsPerMove = 0;
+
     private GUIRenderer guiRenderer;
+    private Evaluator evaluator = new Evaluator();
+    private int aiControllerEvaluationResult = 0;
+    private int playerControllerEvaluationResult = 0;
 
-
-
-    public void preformAction(GameState gameState) {
-
-        for(int i = 0; i < gameState.getGameBoard().getBlackPieces().size(); i++) {
-            GamePiece p = gameState.getGameBoard().getBlackPieces().get(i);
-            if(gameState.getValidActions(p).size() > 1) {
-                Action action = gameState.getValidActions(p).get(0);
-                gameState.preformAction(action);
-
-                //GameState theRightTimeLine = gameState.branchState(action);
-
+    public void preformAction(GameState originalGameState) {
+        numOfStepsPerMove++;
+        for(int i = 0; i < originalGameState.getGameBoard().getBlackPieces().size(); i++) {
+            GamePiece p = originalGameState.getGameBoard().getBlackPieces().get(i);
+            if(originalGameState.getValidActions(p).size() > 1) {
+                Action action = originalGameState.getValidActions(p).get(0);
+                GameState aiControllerGameState = originalGameState.branchState(action);
+                originalGameState.preformAction(action);
+                aiControllerEvaluationResult = evaluator.evaluateGameState(aiControllerGameState, p.getGameTeam());
+                System.out.println(aiControllerEvaluationResult);
 
                 break;
             }
 
-            //
 
+        }
+        numOfStepsPerMove++;
+        if(numOfStepsPerMove == 4){
+            playerControllerEvaluationResult = evaluator.evaluateGameState(originalGameState,GameTeam.WHITE);
+            System.out.println(playerControllerEvaluationResult);
+            System.out.println("-----------------------------");
+            numOfStepsPerMove = 0;
         }
         afterTurn();
     }
@@ -35,8 +48,6 @@ public class AIController {
         guiRenderer.getSelectedAttackAreas().clear();
         guiRenderer.getSelectedMoveAreas().clear();
     }
-
-
     public GUIRenderer getGuiRenderer() {
         return guiRenderer;
     }
