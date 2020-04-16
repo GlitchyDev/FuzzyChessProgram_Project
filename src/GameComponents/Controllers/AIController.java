a5fe17c3db5e3023680af57d1db040fbc8a72dba
evanngevara <44121005+evanngevara@users.noreply.github.com>
2020-04-15 22:01:48 -0400

    Updated AIController to examine 2 turns ahead (4 moves ahead)

--- src/GameComponents/Controllers/AIController.java
+++ src/GameComponents/Controllers/AIController.java
@@ -1,54 +1,101 @@
 package GameComponents.Controllers;
 
 import GameComponents.Board.GameTeam;
+import GameComponents.Board.Pieces.BoardLocation;
 import GameComponents.Board.Pieces.GamePiece;
 import GameComponents.Board.Turn.Action;
+import GameComponents.Board.Turn.AttackAction;
 import GameComponents.GUIRenderer;
 import GameComponents.GameState;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AIController {
 
-    static int numOfStepsPerMove = 0;
-
     private GUIRenderer guiRenderer;
     private Evaluator evaluator = new Evaluator();
-    private int aiControllerEvaluationResult = 0;
-    private int playerControllerEvaluationResult = 0;
 
+    private final int N_MAX = 2;
+    private final int LAYERS_DEEP = 4;
     public void preformAction(GameState originalGameState) {
-        numOfStepsPerMove++;
-        for(int i = 0; i < originalGameState.getGameBoard().getBlackPieces().size(); i++) {
-            GamePiece p = originalGameState.getGameBoard().getBlackPieces().get(i);
-            if(originalGameState.getValidActions(p).size() > 1) {
-                Action action = originalGameState.getValidActions(p).get(0);
-                switch(originalGameState.getTurnActions(originalGameState.getCurrentTurnNumber()).size()) {
-                    case 0:
-                        guiRenderer.recordAction1Text(action.toString());
-                        break;
-                    case 1:
-                        guiRenderer.recordAction2Text(action.toString());
-                        break;
-                }
-                GameState aiControllerGameState = originalGameState.branchState(action);
-                originalGameState.preformAction(action);
-                aiControllerEvaluationResult = evaluator.evaluateGameState(aiControllerGameState, p.getGameTeam());
-                System.out.println(aiControllerEvaluationResult);
 
-                break;
-            }
+        ArrayList<ActionSet> originalActionSet = new ArrayList<>();
+        originalActionSet.add(new ActionSet(evaluator.evaluateGameState(originalGameState,originalGameState.getCurrentTeamTurn()),originalGameState));
+        System.out.println(" ");
 
-
+        ArrayList<ActionSet> bestActions = new ArrayList<>();
+        bestActions.addAll(originalActionSet);
+        for(int i = 0; i < LAYERS_DEEP; i++) {
+            bestActions = getTopNActions(originalGameState.getCurrentTeamTurn(), bestActions);
         }
-        numOfStepsPerMove++;
-        if(numOfStepsPerMove == 4){
-            playerControllerEvaluationResult = evaluator.evaluateGameState(originalGameState,GameTeam.WHITE);
-            System.out.println(playerControllerEvaluationResult);
-            System.out.println("-----------------------------");
-            numOfStepsPerMove = 0;
-        }
+
+        originalGameState.preformAction(bestActions.get(0).getActionList().get(0));
         afterTurn();
+        // Find best value out of best actions list, grab the action set, look at the array index 0 and 1, do both actions, call afterTurn
+
     }
+
+
+    /**
+     * It will return the top N_Max Actionset from the provided actionsets
+     * @return
+     */
+    public ArrayList<ActionSet> getTopNActions(GameTeam evaluatorsTeam, ArrayList<ActionSet> providedActionSet) {
+        ArrayList<ActionSet> resultantActions = new ArrayList<>();
+        //ActionSet[] topActionSets = new ActionSet[N_MAX];
+
+        for(ActionSet actionSet: providedActionSet) {
+            System.out.println("ActionSet size = " + providedActionSet.size());
+            GameTeam gameTeam = actionSet.getResultantGameState().getCurrentTeamTurn();
+            ActionSet[] topActionSets = new ActionSet[N_MAX];
+            // You want to go through every gamepiece of the current players turn, hint, you can cheat and look at whose turn it is
+            for(GamePiece gamePiece: gameTeam == GameTeam.WHITE ? actionSet.getResultantGameState().getGameBoard().getWhitePieces() : actionSet.getResultantGameState().getGameBoard().getBlackPieces()) {
+                for(Action potentialAction: actionSet.getResultantGameState().getValidActions(gamePiece)) {
+                    GameState potentialGameState = actionSet.getResultantGameState().branchState(potentialAction);
+                    int value = evaluator.evaluateGameState(potentialGameState,gameTeam);
+
+                    ActionSet potentialActionSet = new ActionSet(value,potentialGameState,actionSet,potentialAction);
+                    // If its your turn, you do checkGreater, if its your oponents you do check lesser
+                    if(gameTeam == evaluatorsTeam) {
+                        checkGreater(topActionSets, potentialActionSet);
+                    } else {
+                        checkLesser(topActionSets,potentialActionSet);
+                    }
+                }
+            }
+            resultantActions.addAll(Arrays.asList(topActionSets));
+        }
+        System.out.println("--------------->resultantActions size = " + resultantActions.size());
+
+        return resultantActions;
+    }
+
+    public void checkGreater(ActionSet[] topActionSet, ActionSet actionSet) {
+        if(actionSet != null) {
+            for (int i = 0; i < topActionSet.length; i++) {
+                if (topActionSet[i] == null || actionSet.getEvaluationResult() > topActionSet[i].getEvaluationResult()) {
+                    ActionSet oldActionSet = topActionSet[i];
+                    topActionSet[i] = actionSet;
+                    checkGreater(topActionSet, oldActionSet);
+                    return;
+                }
+            }
+        }
+
+    }
+    public void checkLesser(ActionSet[] topActionSet, ActionSet actionSet) {
+        if(actionSet != null) {
+            for (int i = 0; i < topActionSet.length; i++) {
+                if (topActionSet[i] == null || actionSet.getEvaluationResult() < topActionSet[i].getEvaluationResult()) {
+                    ActionSet oldActionSet = topActionSet[i];
+                    topActionSet[i] = actionSet;
+                    checkGreater(topActionSet, oldActionSet);
+                    return;
+                }
+            }
+        }
+    }
+
 
     private void afterTurn() {
         guiRenderer.getSelectedPieces().clear();
